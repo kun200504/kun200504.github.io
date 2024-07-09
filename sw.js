3c677f14e20673a88f488c97fc6d83cd09c7b7c6
kun200504 <214234734@qq.com>
2024-07-09 16:08:56 +0800

    Site updated: 2024-07-09 16:08:56

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1720512098258";
+const VERSION = "1720512516740";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
